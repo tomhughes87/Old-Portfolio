e3aa8a346f4d2ef6e02acb8fcf153364e3ad03d0
Tom Hughes <tomhughes1987@gmail.com>
2022-06-17 18:58:21 +0200

    ADD COMPS to app.tsx

--- toms-portfolio/src/App.tsx
+++ toms-portfolio/src/App.tsx
@@ -1,28 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './MyHeader.css';
+import './Cardflip.css'
 import ComponentBlank from './components/ComponentBlank';
+import MyHeader from './components/layout/MyHeader';
+import Cardflip from './components/Cardflip'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <MyHeader />
 
-        <ComponentBlank />
-      </header>
+    <div className="App">
+
+      <div className='App-Main-Body'>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Cardflip />  
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+
+          <ComponentBlank />
+        </header>
+      </div>
+
     </div>
+    </>
   );
 }
 
